4f4da81453353b168f4a9478a103564f0cd59d56
kyoo-al <alenananabel2005@mail.ru>
2024-12-14 02:06:27 +1000

    6 лабораторная

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -1,18 +1,17 @@
 <?php
-
 namespace App\Controller;
-
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
     public function index(): Response
     {
+        $user = $this->getUser();
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'user' => $user,
         ]);
     }
 }
